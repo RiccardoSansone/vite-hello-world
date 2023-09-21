d9755eb6ef3683c4b20cd1ecd0b346a50157361c
RiccardoSansone <r.sansone@gmail.com>
2023-09-21 17:06:42 +0200

    hello vite

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+export default{
+  data() {
+    return {
+      hello: "Hello vite"
+    }
+  }
+}
+</script>
+
+<template>
+  
+  <h1>{{ hello }}</h1>
+
+</template>
+
+<style scoped>
+
+</style>
